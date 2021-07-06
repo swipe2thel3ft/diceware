49b1dce739967c5601527faa90a1686aa6a66ed2
Nick Bennett <nick@nickbenn.com>
2021-07-06 11:03:28 -0600

    Add service interfaces.

---
+++ src/main/java/edu/cnm/deepdive/diceware/service/PassphraseGenerator.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.diceware.service;
+
+public interface PassphraseGenerator {
+
+  String[] generate(int length);
+
+}
---
+++ src/main/java/edu/cnm/deepdive/diceware/service/WordListProvider.java
@@ -0,0 +1,9 @@
+package edu.cnm.deepdive.diceware.service;
+
+import java.util.List;
+
+public interface WordListProvider {
+
+  List<String> getWords();
+
+}
