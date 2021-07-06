41379209f3d625ad8de39f85e572721290655e9d
Nick Bennett <nick@nickbenn.com>
2021-07-06 10:02:13 -0600

    Extract contents.

--- src/main/java/edu/cnm/deepdive/diceware/controller/HelloWorldController.java
+++ src/main/java/edu/cnm/deepdive/diceware/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package edu.cnm.deepdive.diceware.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,22 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloWorldController {
 
+  private static final String GREETING_FORMAT = "Hello, %s!";
+  private static final String COMPLIMENT_FORMAT = "Good job in making it to week %d! You now know much more Java than 99+%% of the population.";
+  private static final String EXPLANATION = "This is a simple Spring MVC-based web service.";
+
   @GetMapping("greet")
   public String greet(
       @RequestParam(name = "t", defaultValue = "world") String target) {
-    return String.format("Hello, %s!", target);
+    return String.format(GREETING_FORMAT, target);
   }
 
   @GetMapping("compliment")
   public String beNice(@RequestParam(name = "w", defaultValue = "5") int week) {
-    return String.format(
-        "Good job in making it to week %d! You now know much more Java than 99+%% of the population.",
-        week);
+    return String.format(COMPLIMENT_FORMAT, week);
   }
 
   @GetMapping
   public String explain() {
-    return "This is a simple Spring MVC-based web service.";
+    return EXPLANATION;
   }
 
 }
