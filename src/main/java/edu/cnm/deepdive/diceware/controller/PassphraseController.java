42e8afd93c38186ecc0ade0d98f8dedfabec6aad
Victor Perez <victorperez099@yahoo.com>
2021-07-06 14:46:45 -0600

    Transient passphrase generation working.

--- src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/diceware/controller/PassphraseController.java
@@ -4,6 +4,7 @@ import edu.cnm.deepdive.diceware.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +19,7 @@ public class PassphraseController {
   }
 
   @GetMapping("/transient")
-  public String[] generate(int length) {
+  public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
   }
 
---
+++ src/main/java/edu/cnm/deepdive/diceware/service/DicewareGenerator.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.diceware.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DicewareGenerator implements PassphraseGenerator {
+
+  private final List<String> words;
+  private final Random rng;
+
+  public DicewareGenerator(WordListProvider provider, Random rng) {
+    this.words = new ArrayList<>(provider.getWords());
+    this.rng = rng;
+  }
+
+  @Override
+  public String[] generate(int length) {
+    String[] passphrase = new String[length];
+    for (int i = 0; i < length; i++) {
+      passphrase[i] = words.get(rng.nextInt(words.size()));
+    }
+    return passphrase;
+  }
+
+
+}
